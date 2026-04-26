f46a3993ab67d5edc0d62853f57a87bba3853eea
Tim Lappe <timlappe@proton.me>
2026-04-26 20:04:12 +0200

    Reset migrations

---
+++ backend/migrations/Version20260426180358.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260426180358 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE printer_device (printer_device_id UUID NOT NULL, driver_code VARCHAR(64) NOT NULL, connection JSON NOT NULL, print_settings JSON NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY (printer_device_id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE printer_device');
+    }
+}
